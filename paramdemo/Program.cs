33a19c50d23cfb1c8331af47e63b047f3b30a874
sandra lakshmi <skeerthisandra@gmail.com>
2022-08-26 18:07:40 +0530

    Add project files.

---
+++ paramdemo/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleAppdemo
+{
+    internal class parasDemo
+    {
+        public static int Sum(params int[] arr)
+        {
+            int sum = 0;
+            foreach (int i in arr)
+                sum += i;
+            return sum;
+
+        }
+        public static void Main()
+        {
+            Console.WriteLine(Sum(1, 2, 3));
+            Console.WriteLine(Sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+            int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            Console.WriteLine(Sum(a));
+        }
+    }
+}
